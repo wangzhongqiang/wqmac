473ed21a206bc174bcd9df75a7bfc05d104c3e7b
wangzhongqiang <Mac@MacdeMac-mini.local>
2017-01-05 22:00:47 +0800

    change

--- wqmac/settings.py
+++ wqmac/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'wqmac.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "geoparadox",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
