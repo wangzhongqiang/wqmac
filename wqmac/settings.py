e020aba7a5348afa9f73810742f7a8f5710f0805
wangzhongqiang <Mac@MacdeMac-mini.local>
2017-01-05 22:12:50 +0800

    change

--- wqmac/settings.py
+++ wqmac/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(h2cr^%x1u=9v2_%kwpmyujupf0(v)e029q1dmuj-qy@l0lb@-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['101.200.224.106']
 
 
 # Application definition
